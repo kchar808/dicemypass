30a8bf9eabfea04de29dadf5db1f94b8bef1e46a
Keelana Char <keelanachar@gmail.com>
2020-01-26 00:03:40 -0600

    added passphrase gen test

--- lib/dicemypass.rb
+++ lib/dicemypass.rb
@@ -1,7 +1,20 @@
 require "dicemypass/version"
 
 module Dicemypass
-  def self.say_hi(name)
-    "Hello, #{name}!"
+  # First we load the absolute path of our eff_long_wordlist.txt.
+  @eff_wordlist = File.dirname(__FILE__) + '/dicemypass/assets/eff_long_wordlist.txt'
+
+  # The default passphrase length should be 7
+  def self.gen_passphrase(pass_length = 7)
+
+    # Read filename eff_long_wordlist and save it as a list.
+    wordlist = File.readlines(@eff_wordlist)
+
+    # Strip the '\n' out of every line.
+    wordlist.map(&:strip!)
+
+    # Shuffle the list and return a list up to pass_length words
+    # which in the case would be equal to 7 words.
+    wordlist.shuffle[0...pass_length]
   end
 end
--- test/dicemypass_test.rb
+++ test/dicemypass_test.rb
@@ -2,11 +2,24 @@ require "test_helper"
 require 'dicemypass'
 
 class DicemypassTest < Minitest::Test
+  # def setup; end
+
   def test_that_it_has_a_version_number
     refute_nil ::Dicemypass::VERSION
   end
 
-  def test_say_hi
-    assert_equal Dicemypass.say_hi('Keelana'), 'Hello, Keelana!'
+  # def test_gen_passphrase; end
+  def test_gen_passphrase
+    # gen_passphrase should generate and respect prassphrase length
+    passphrase3 = Dicemypass.gen_passphrase(3)
+    passphrase_default = Dicemypass.gen_passphrase
+    passphrase12 = Dicemypass.gen_passphrase(12)
+    assert_equal passphrase3.length, 3, 'Passphrase length != 3'
+    assert_equal passphrase_default.length, 7, 'Passphrase length != 7'
+    assert_equal passphrase12.length, 12, 'Passphrase length != 12'
   end
+
+  # def test_vulnerable_pass; end
+
+  # def test_secure_pass; end
 end
